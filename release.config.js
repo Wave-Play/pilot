ef91e3714ef296d6750b029d11e14141d5eac4b0
Pkmmte Xeleon <hi@pkmmte.com>
2022-10-16 21:20:42 -0700

    chore(pkg): semantic-release plugins
    Added various plugins to hopefully automate certain things such as enforcing commit convention, package version updates, and generating release notes.

---
+++ release.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+	plugins: [
+		'@semantic-release/commit-analyzer',
+		'@semantic-release/release-notes-generator',
+		['@semantic-release/git', {
+      assets: ['CHANGELOG.md', 'dist/**/*.{js}', 'package.json'],
+      message: `chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}`
+    }]
+	],
+	release: {
+		branches: ['main', 'next']
+	},
+	repositoryUrl: 'https://github.com/Wave-Play/pilot'
+};
