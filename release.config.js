e0f3e26126a805bbcf9e712c6482e951721ed2d5
Pkmmte Xeleon <hi@pkmmte.com>
2022-10-16 22:44:41 -0700

    fix(ci): removed semantic-release/git options
    I was wrong about the npm plugin in the previous commit...

--- release.config.js
+++ release.config.js
@@ -3,10 +3,7 @@ module.exports = {
 		'@semantic-release/commit-analyzer',
 		'@semantic-release/release-notes-generator',
 		'@semantic-release/npm',
-		['@semantic-release/git', {
-      assets: ['CHANGELOG.md', 'dist/**/*.{js}', 'package.json'],
-      message: `chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}`
-    }]
+		'@semantic-release/git'
 	],
 	release: {
 		branches: ['main', 'next', 'beta', 'alpha']
