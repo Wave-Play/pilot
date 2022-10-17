37d55e0145865ffab5885bdd0a3cddc01d8450ca
Pkmmte Xeleon <hi@pkmmte.com>
2022-10-16 22:42:24 -0700

    fix(ci): use semantic-release/npm plugin
    I think the documentation implies this being necessary to generate the "nextRelease" variable used by the git plugin.

--- release.config.js
+++ release.config.js
@@ -2,6 +2,7 @@ module.exports = {
 	plugins: [
 		'@semantic-release/commit-analyzer',
 		'@semantic-release/release-notes-generator',
+		'@semantic-release/npm',
 		['@semantic-release/git', {
       assets: ['CHANGELOG.md', 'dist/**/*.{js}', 'package.json'],
       message: `chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}`
