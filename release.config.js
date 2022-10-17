9aba2827ea325c08b39240c27eedc4b3102aca44
Pkmmte Xeleon <hi@pkmmte.com>
2022-10-16 23:03:43 -0700

    fix(ci): re-enabled semantic-release plugins
    Now that we know what caused the issues...

--- release.config.js
+++ release.config.js
@@ -1,6 +1,12 @@
 module.exports = {
+	plugins: [
+		'@semantic-release/commit-analyzer',
+		'@semantic-release/release-notes-generator',
+		'@semantic-release/npm',
+		'@semantic-release/git'
+	],
 	release: {
-		branches: ['master', 'beta', 'alpha']
+		branches: ['master', 'next', 'beta', 'alpha']
 	},
 	repositoryUrl: 'https://github.com/Wave-Play/pilot'
 };
